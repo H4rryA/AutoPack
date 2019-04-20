8ac59cc5dead53a109474f0743f4a223a8cd3cbd
h4rrya <harryarakkal@gmail.com>
2019-04-20 18:22:09 -0500

    Add delete option and fixed duplicate alerts.

--- AutoPack/BackpackViewController.swift
+++ AutoPack/BackpackViewController.swift
@@ -24,15 +24,7 @@ class BackpackViewController: UIViewController {
             tableView.reloadData()
         }
     }
-    private var itemDict: [[String: Any]]? {
-        didSet {
-            print(itemDict ?? [])
-            items = [[],[],[]]
-            for item in itemDict! {
-                items[item["status"] as! Int].append(Item(rfid: item["RFID"] as! String, name: item["name"] as! String))
-            }
-        }
-    }
+    private var itemDict: [[String: Any]]?
     private var itemsResource: Resource? {
         didSet {
             oldValue?.removeObservers(ownedBy: self)
@@ -42,6 +34,7 @@ class BackpackViewController: UIViewController {
                 .loadIfNeeded()
         }
     }
+    private var postRequest: Request?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,17 +53,53 @@ class BackpackViewController: UIViewController {
     private func setupTable() {
         items = [[],[],[]]
         tableView.dataSource = self
+        tableView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        updateItems()
+    }
+    
+    private func updateItems() {
         itemsResource = ItemAPI.sharedInstance.itemList()
+    }
+    
+    private func newItemAlert(pocket: Int, rfid: String) {
+        let alert = UIAlertController(title: "You have a new item!", message: "What would you like to call it?", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = rfid
+            textField.text = "New Item"
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0]
+            let input = textField?.text ?? rfid
+            self.postRequest = self.itemsResource?.request(.put, urlEncoded: ["rfid": rfid, "name": (input != "") ? input : rfid])
+                .onSuccess({ (_) in
+                    self.updateItems()
+                })
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
 extension BackpackViewController: ResourceObserver {
     func resourceChanged(_ resource: Resource, event: ResourceEvent) {
-        itemDict = resource.jsonArray as? [[String: Any]] ?? []
+        if case .newData = event {
+            itemDict = resource.jsonArray as? [[String: Any]] ?? []
+            print(itemDict ?? [])
+            var tempItems = [[Item](),[Item](),[Item]()]
+            for item in itemDict! {
+                let name = item["name"] as! String
+                let pocket = item["status"] as! Int
+                let rfid = item["RFID"] as! String
+                tempItems[pocket].append(Item(rfid: rfid, name: name))
+                if name == "New Item" {
+                    newItemAlert(pocket: pocket, rfid: rfid)
+                }
+            }
+            items = tempItems
+        }
     }
 }
 
@@ -108,6 +137,19 @@ extension BackpackViewController: UITableViewDataSource {
         return items?.count ?? 2
     }
     
+}
+
+extension BackpackViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler: (Bool) -> Void) in
+            let _ = self.itemsResource?.request(.delete, urlEncoded: ["rfid": self.items[indexPath.section][indexPath.row].rfid])
+                .onSuccess({ (entity) in
+                    self.items[indexPath.section].remove(at: indexPath.row)
+                })
+        }
+        let swipeConfig = UISwipeActionsConfiguration(actions: [deleteAction])
+        return swipeConfig
+    }
 }
 
 extension BackpackViewController: EventVCDelegate {
